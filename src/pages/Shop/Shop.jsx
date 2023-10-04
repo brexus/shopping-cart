e6d5de193ae198b5260af5b47b99eefbbe545166
brexus <92671269+brexus@users.noreply.github.com>
2023-10-04 23:45:17 +0200

    rebuild structure of website

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,15 +1,13 @@
 import { Outlet } from "react-router-dom";
-// import "./Shop.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
 export default function Shop() {
 
-
     return (
         <div id="shop">
             <Header />
-
+    
             <Outlet />
 
             <Footer />
